4be2786e5fd38fb041930ea20e56872d6cbd5681
Marc Aubreville <marc.aubreville@hs-flensburg.de>
2025-05-30 13:19:53 +0200

    Changed setup.py for EXACT-Sync.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="EXCAT-Sync", 
+    name="EXACT-Sync", 
     version="0.0.39",
-    author="Christian Marzahl",
-    author_email="christian.marzahl@gamil.com",
-    description="A package to download images and annotations from the EXACT Server https://github.com/ChristianMarzahl/Exact",
+    author="Christian Marzahl, Marc Aubreville",
+    author_email="marc.aubreville@hs-flensburg.de",
+    description="A package to download images and annotations from the EXACT Server https://github.com/DeepMicroscopy/Exact",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ChristianMarzahl/EXACT-Sync",
+    url="https://github.com/DeepMicroscopy/EXACT-Sync",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests', 
